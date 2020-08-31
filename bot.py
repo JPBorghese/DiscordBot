58e551d2ea76d948f81f4e22c82c9dfc26c7d8e1
JBorghese <33209740+JBorghese@users.noreply.github.com>
2020-08-31 17:10:07 -0400

    added commands to print text

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 
-import discord, sys
+import discord, sys, re
 from commands import *
 
 if len(sys.argv) != 2 :
@@ -11,13 +11,28 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     print(message.author.display_name + ":\n\t" + message.content)
+
+    #commands
     if (message.content.startswith('$')):
         command = message.content[1:len(message.content):1]
-        try:
-            await eval(command + '.' + command + '(message)')
-            print(command + '.' + commnad + '(message)')
-        except:
-            pass
+        if(re.match("^[a-zA-Z_]+$", command)):
+            try:
+                await eval(command + '.' + command + '(message)')
+            except:
+                pass
+        else:
+            print('invalid command : ' + command)
+    #text
+    elif (message.content.startswith('!')):
+        command = message.content[1:len(message.content):1]
+        if(re.match("^[a-zA-Z_]+$", command)):
+            try:
+                await message.delete()
+                f = open('pasta/' + command + '.txt', 'r', encoding='utf-8')
+                await message.channel.send(f.read())
+                f.close()
+            except:
+                pass
 
 @client.event
 async def on_reaction_add(reaction, user):
--- commands/__init__.py
+++ commands/__init__.py
@@ -1,2 +1,2 @@
 #list all file names here
-__all__ = ["delete_all", "hmm"]
+__all__ = ["delete_all"]
