e59bb680cdcf528d0015ccbba5a26b2b6e9ff4cc
JBorghese <33209740+JBorghese@users.noreply.github.com>
2020-09-01 20:26:20 -0400

    added warning message for commands add_role and remove_role

--- commands/add_role.py
+++ commands/add_role.py
@@ -5,8 +5,13 @@ async def add_role(stuff):
     if len(stuff["message"].content.split()) > 1:
         newRole = stuff["message"].content.split()[1]
         roles = stuff["message"].channel.guild.roles
+        added = False
 
         for role in roles:
             if role.name == newRole:
                 await stuff["message"].author.add_roles(role)
+                added = True
                 break
+
+        if not added:
+            await stuff["message"].channel.send("Invalid Role: " + newRole)
--- commands/remove_role.py
+++ commands/remove_role.py
@@ -5,8 +5,13 @@ async def remove_role(stuff):
     if len(stuff["message"].content.split()) > 1:
         newRole = stuff["message"].content.split()[1]
         roles = stuff["message"].channel.guild.roles
+        deleted = False
 
         for role in roles:
             if role.name == newRole:
                 await stuff["message"].author.remove_roles(role)
+                deleted = True
                 break
+
+        if not deleted:
+            await stuff["message"].channel.send("Invalid Role: " + newRole)
