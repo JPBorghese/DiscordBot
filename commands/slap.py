63cd558bc49a9e022cce9ba11fa494e16ec70374
JBorghese <33209740+JBorghese@users.noreply.github.com>
2020-09-03 02:02:00 -0400

    updated slap

--- commands/slap.py
+++ commands/slap.py
@@ -1,24 +1,40 @@
-
+import discord
 
 async def slap(message, client):
     messageArray = message.content.split()
 
     #check more than 1 'token'
-    if len(messageArray) == 2:
+    if len(messageArray) >= 2:
         mentions = message.mentions
 
         #format output string
         if len(mentions) > 0:
-            botMessage = message.author.mention + " slapped"
+            botMessage = "<@" + str(message.author.id) + "> slapped "
+
+            length = len(mentions)
+            commaCount = length - 2
+            andCount = 0
+            if length >= 2:
+                andCount = 1
+
             for i in range(len(mentions)):
-                if len(mentions) == 1:
-                    botMessage += " "
-                elif i == len(mentions) - 1:
-                    botMessage += " and "
-                elif i != 0:
+                botMessage += "<@" + str(mentions[i].id) + ">"
+
+                if commaCount > 0:
+                    commaCount -= 1
                     botMessage += ", "
+                    continue
+                elif andCount > 0:
+                    andCount -= 1
+                    botMessage += " and "
+                else:
+                    botMessage += " in the face!"
 
-            botMessage += mentions[i].mention + " in the face!"
+            embed = discord.Embed(
+                title = "",
+                color = 0xff9933,
+                description = botMessage
+            )
 
-            await message.channel.send(botMessage)
+            await message.channel.send(embed=embed)
 
